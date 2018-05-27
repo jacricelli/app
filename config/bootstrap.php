4ac7475314495ae20fffe55af0b0502ad6339d42
Jorge Alberto Cricelli <jacricelli@gmail.com>
2018-05-27 02:09:04 -0300

    Actualizar el arranque de la aplicación

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,171 +12,102 @@
  * @since         0.10.8
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-
-/*
- * Configure paths required to find CakePHP + general filepath constants
- */
-require __DIR__ . '/paths.php';
-
-/*
- * Bootstrap CakePHP.
- *
- * Does the various bits of setup that CakePHP needs to do.
- * This includes:
- *
- * - Registering the CakePHP autoloader.
- * - Setting the default application paths.
- */
-require CORE_PATH . 'config' . DS . 'bootstrap.php';
-
 use Cake\Cache\Cache;
 use Cake\Console\ConsoleErrorHandler;
-use Cake\Core\App;
 use Cake\Core\Configure;
-use Cake\Core\Configure\Engine\PhpConfig;
-use Cake\Core\Plugin;
 use Cake\Database\Type;
 use Cake\Datasource\ConnectionManager;
 use Cake\Error\ErrorHandler;
-use Cake\Http\ServerRequest;
 use Cake\Log\Log;
-use Cake\Mailer\Email;
-use Cake\Utility\Inflector;
 use Cake\Utility\Security;
 
-/*
- * Read configuration file and inject configuration into various
- * CakePHP classes.
- *
- * By default there is only one configuration file. It is often a good
- * idea to create multiple configuration files, and separate the configuration
- * that changes from configuration that does not. This makes deployment simpler.
+/**
+ * Definir las rutas de acceso
+ */
+require __DIR__ . '/paths.php';
+
+/**
+ * Ejecutar el arranque de CakePHP
+ */
+require CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+/**
+ * Cargar el archivo de configuración
  */
 try {
-    Configure::config('default', new PhpConfig());
     Configure::load('app', 'default', false);
-} catch (\Exception $e) {
+} catch (Exception $e) {
     exit($e->getMessage() . "\n");
 }
 
-/*
- * Load an environment local configuration file.
- * You can use a file like app_local.php to provide local overrides to your
- * shared configuration.
+/**
+ * Configurar el manejo de errores y excepciones
  */
-//Configure::load('app_local', 'default');
+if (PHP_SAPI !== 'cli') {
+    (new ErrorHandler(Configure::read('Error')))->register();
+} else {
+    (new ConsoleErrorHandler(Configure::read('Error')))->register();
 
-/*
- * When debug = true the metadata cache should only last
- * for a short time.
+    Configure::write('Log.debug.file', 'cli-debug');
+    Configure::write('Log.error.file', 'cli-error');
+}
+
+/**
+ * Establecer la duración de la caché de metadatos y rutas
  */
 if (Configure::read('debug')) {
     Configure::write('Cache._cake_model_.duration', '+2 minutes');
     Configure::write('Cache._cake_core_.duration', '+2 minutes');
-    // disable router cache during development
-    Configure::write('Cache._cake_routes_.duration', '+2 seconds');
+    Configure::write('Cache._cake_routes_.duration', '+5 seconds');
 }
 
-/*
- * Set the default server timezone. Using UTC makes time calculations / conversions easier.
- * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
+/**
+ * Establecer la zona horaria predeterminada
  */
 date_default_timezone_set(Configure::read('App.defaultTimezone'));
 
-/*
- * Configure the mbstring extension to use the correct encoding.
+/**
+ * Establecer la codificación de caracteres interna
  */
 mb_internal_encoding(Configure::read('App.encoding'));
 
-/*
- * Set the default locale. This controls how dates, number and currency is
- * formatted and sets the default language to use for translations.
+/**
+ * Establecer la localidad predeterminada
  */
 ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
 
-/*
- * Register application error and exception handlers.
+/**
+ * Configuración de la caché
  */
-$isCli = PHP_SAPI === 'cli';
-if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
-} else {
-    (new ErrorHandler(Configure::read('Error')))->register();
-}
-
-/*
- * Include the CLI bootstrap overrides.
- */
-if ($isCli) {
-    require __DIR__ . '/bootstrap_cli.php';
-}
-
-/*
- * Set the full base URL.
- * This URL is used as the base of all absolute links.
- *
- * If you define fullBaseUrl in your config file you can remove this.
- */
-if (!Configure::read('App.fullBaseUrl')) {
-    $s = null;
-    if (env('HTTPS')) {
-        $s = 's';
-    }
-
-    $httpHost = env('HTTP_HOST');
-    if (isset($httpHost)) {
-        Configure::write('App.fullBaseUrl', 'http' . $s . '://' . $httpHost);
-    }
-    unset($httpHost, $s);
-}
-
 Cache::setConfig(Configure::consume('Cache'));
+
+/**
+ * Configuración de las conexiones a bases de datos
+ */
 ConnectionManager::setConfig(Configure::consume('Datasources'));
-Email::setConfigTransport(Configure::consume('EmailTransport'));
-Email::setConfig(Configure::consume('Email'));
+
+/**
+ * Configuración del registro de mensajes
+ */
 Log::setConfig(Configure::consume('Log'));
+
+/**
+ * Configuración de seguridad
+ */
 Security::setSalt(Configure::consume('Security.salt'));
 
-/*
- * The default crypto extension in 3.0 is OpenSSL.
- * If you are migrating from 2.x uncomment this code to
- * use a more compatible Mcrypt based implementation
+/**
+ * Habilitar objetos de tiempo inmutables en el ORM
  */
-//Security::engine(new \Cake\Utility\Crypto\Mcrypt());
-
-/*
- * Enable immutable time objects in the ORM.
- *
- * You can enable default locale format parsing by adding calls
- * to `useLocaleParser()`. This enables the automatic conversion of
- * locale specific date formats. For details see
- * @link https://book.cakephp.org/3.0/en/core-libraries/internationalization-and-localization.html#parsing-localized-datetime-data
- */
+/* @noinspection PhpUndefinedMethodInspection */
 Type::build('time')
     ->useImmutable();
+/* @noinspection PhpUndefinedMethodInspection */
 Type::build('date')
     ->useImmutable();
+/* @noinspection PhpUndefinedMethodInspection */
 Type::build('datetime')
     ->useImmutable();
+/* @noinspection PhpUndefinedMethodInspection */
 Type::build('timestamp')
     ->useImmutable();
-
-/*
- * Custom Inflector rules, can be set to correctly pluralize or singularize
- * table, model, controller names or whatever other string is passed to the
- * inflection functions.
- */
-//Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
-//Inflector::rules('irregular', ['red' => 'redlings']);
-//Inflector::rules('uninflected', ['dontinflectme']);
-//Inflector::rules('transliteration', ['/å/' => 'aa']);
-
-/*
- * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
- * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
- * advanced ways of loading plugins
- *
- * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('Migrations'); //Loads a single plugin named Migrations
- *
- */
