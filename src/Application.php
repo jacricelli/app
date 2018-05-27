929da9e826187c0e34e23f8dbceb82c138d5f804
Jorge Alberto Cricelli <jacricelli@gmail.com>
2018-05-26 23:37:46 -0300

    Refactorizar clase de la aplicación

--- src/Application.php
+++ src/Application.php
@@ -14,49 +14,46 @@
  */
 namespace App;
 
-use Cake\Core\Configure;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Http\BaseApplication;
-use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
+use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\Router;
 
 /**
- * Application setup class.
- *
- * This defines the bootstrapping logic and middleware layers you
- * want to use in your application.
+ * Aplicación
  */
 class Application extends BaseApplication
 {
     /**
-     * {@inheritDoc}
+     * Define las rutas para una aplicación
+     *
+     * @param \Cake\Routing\RouteBuilder $routes Objeto utilizado para construir rutas
+     *
+     * @return void
      */
-    public function bootstrap()
+    public function routes($routes)
     {
-        // Call parent to load bootstrap from files.
-        parent::bootstrap();
+        if (!Router::$initialized) {
+            Router::$initialized = true;
+
+            $routes->connect('/', 'App::home');
+            $routes->fallbacks(DashedRoute::class);
+        }
     }
 
     /**
-     * Setup the middleware queue your application will use.
+     * Define las capas de middleware HTTP para una aplicación
      *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
-     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue La cola de middleware
+     * para configurar en la clase de aplicación
+     *
+     * @return \Cake\Http\MiddlewareQueue
      */
     public function middleware($middlewareQueue)
     {
         $middlewareQueue
-            // Catch any exceptions in the lower layers,
-            // and make an error page/response
             ->add(ErrorHandlerMiddleware::class)
-
-            // Handle plugin/theme assets like CakePHP normally does.
-            ->add(AssetMiddleware::class)
-
-            // Add routing middleware.
-            // Routes collection cache enabled by default, to disable route caching
-            // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
-            // you might want to disable this cache in case your routing is extremely simple
             ->add(new RoutingMiddleware($this, '_cake_routes_'));
 
         return $middlewareQueue;
